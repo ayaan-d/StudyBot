ef947b76229068a93adc0f926b63b66885de988d
mizna-d <miznadada@gmail.com>
2020-12-25 21:14:08 -0500

    add question works but doesnt store yet

--- bot/discordBot.py
+++ bot/discordBot.py
@@ -13,24 +13,36 @@ async def test(ctx):
     await ctx.send('its working')
 
 
-# noinspection SpellCheckingInspection
-@client.command(aliases=["addquestion"])
+@client.command()
 async def add_question(ctx, question=None):
-    if question is not None:
-        question = await client.wait_for('Please input the question you would '
-                                         'like to add to the question bank')
 
-    answer = await client.wait_for('Please input the correct answer for '
-                                   'the inputted question')
+    def check(m):
+        print("checks")
+        return m.content is not None
+
+    if question is None:
+        print("if statement")
+        await ctx.send('Please input the question you would like to add to the '
+                       'question bank')
+        question = await client.wait_for('message', check=check)
+    await ctx.send('Please input the correct answer for the previously'
+                   ' inputted question')
+    answer = await client.wait_for('message', check=check)
     # add question to csv
     # add answer to csv
     # add channel to csv
-    await ctx.send(f"{question} and its answer, '{answer}' was successfully "
-                   f"added to the question bank")
+    await ctx.send(f"The question, '{question.content}' and its answer, '{answer.content}' was "
+                   f"successfully added to the question bank")
+
 
 @client.command()
-async def remove_question():
-
+async def remove_question(ctx):
+    # print all the questions in the question bank with numbers
+    # remove accordingly
+    question = ""
+    answer = ""
+    await ctx.send(f"{question} and its answer, '{answer}'"
+                   f"was successfully removed from the question bank")
 
 
 client.run('Nzc1Nzc0OTc2NTQ4NDA1Mjc4.X6rOvw.IIMUROaQt26-ztNsZpPNM4gL2gA')
